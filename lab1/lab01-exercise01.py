5a8e91cad6a4a51f48cb10071ba1035694a2acf9
Aiden Dorantes Barnard <apbarnard2004@icloud.com>
2023-11-08 15:34:31 -0600

    Lab1

---
+++ lab1/lab01-exercise01.py
@@ -0,0 +1,10 @@
+'''
+Author: Aiden Barnard
+KUID: 3108416
+Date: Thursday, September 7, 2023
+Lab: lab01
+Last modified: Thursday, September 7, 2023
+Purpose: This exercise simply prints the phrase, "This is my first lab exercise!"
+'''
+
+print('This is my first lab exercise!')
---
+++ lab1/lab01-exercise02.py
@@ -0,0 +1,13 @@
+'''
+Author: Aiden Barnard
+KUID: 3108416
+Date: Thursday, September 7, 2023
+Lab: lab01
+Last modified: Thursday, September 7, 2023
+Purpose: This exercise prints my name, major, and hobbies.
+'''
+
+print('My name is Aiden Barnard')
+print('I am a EECS computer science major')
+print('My hobbies are:\n\tCoding \n\tCooking \n\tHanging out with my girlfriend \n\tShopping \n\tListening to music, like Ye or Bad Bunny.')
+print('Goodbye')
