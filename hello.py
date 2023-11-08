9a0787623282758a427d77f91f89f447c9892292
Aiden Dorantes Barnard <142916831+TheOfficialBarn@users.noreply.github.com>
2023-11-08 15:31:47 -0600

    My first Python code!

---
+++ hello.py
@@ -0,0 +1,10 @@
+'''
+Author: Aiden Barnard
+KUID: 3108416
+Date: Thursday, September 7, 2023
+Lab: lab01
+Last modified: Thursday, September 7, 2023
+Purpose: This exercise is my first; it is an oppertunity to practice uploading to GitHub.
+'''
+
+print("Hello, World!")
